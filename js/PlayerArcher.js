9f75045168520935aff01ff73003a83fc3d5aa9d
byjusmx2 <87038490+byjusmx2@users.noreply.github.com>
2021-10-13 15:42:41 -0500

    Update PlayerArcher.js

--- js/PlayerArcher.js
+++ js/PlayerArcher.js
@@ -12,7 +12,7 @@ class PlayerArcher {
 
     World.add(world, this.body);
 
-    Matter.Body.setAngle(this.body, -PI / 2); // -90 degree
+    Matter.Body.setAngle(this.body, -PI / 2); // -90 grados
   }
 
   display() {
